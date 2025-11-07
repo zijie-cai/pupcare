2d6a2bf662d30e5d5d5414f10360f0ca72e8c7de
Zijie Cai <leo20020208@gmail.com>
2025-11-07 00:38:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/pupcare/',
+    base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
