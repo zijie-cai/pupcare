011c9af383e8f88613374da53b98ebafdba9ada2
Zijie Cai <leo20020208@gmail.com>
2025-12-11 13:42:58 -0500

    dw

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/pupcare/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
